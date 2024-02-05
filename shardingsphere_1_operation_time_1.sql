501f513607da9498ccb79da44d3eeeeed9551b50
gavin gu <guangyin_gu@sina.com>
2024-02-05 11:42:59 +0800

    Add files via upload

---
+++ shardingsphere_1_operation_time_1.sql
@@ -0,0 +1,67 @@
+-- MySQL dump 10.13  Distrib 8.0.32, for Win64 (x86_64)
+--
+-- Host: hitachi-iot-test-rds.cath6q1djhtk.rds.cn-northwest-1.amazonaws.com.cn    Database: shardingsphere_1
+-- ------------------------------------------------------
+-- Server version	8.0.33
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+SET @MYSQLDUMP_TEMP_LOG_BIN = @@SESSION.SQL_LOG_BIN;
+SET @@SESSION.SQL_LOG_BIN= 0;
+
+--
+-- GTID state at the beginning of the backup 
+--
+
+SET @@GLOBAL.GTID_PURGED=/*!80000 '+'*/ '';
+
+--
+-- Table structure for table `operation_time_1`
+--
+
+DROP TABLE IF EXISTS `operation_time_1`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `operation_time_1` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `count` bigint DEFAULT NULL,
+  `created_at` datetime NOT NULL,
+  `expansion_valve` int NOT NULL,
+  `power` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci DEFAULT NULL,
+  `thing_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  `unit_id` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `operation_time_index_optimized` (`unit_id`,`thing_id`,`created_at`),
+  KEY `operation_time_index_for_dataprocessing` (`power`,`unit_id`,`thing_id`,`created_at`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `operation_time_1`
+--
+
+LOCK TABLES `operation_time_1` WRITE;
+/*!40000 ALTER TABLE `operation_time_1` DISABLE KEYS */;
+INSERT INTO `operation_time_1` VALUES (1,2,'2024-01-24 02:57:18',0,'0','2','2'),(2,5,'2024-01-26 08:06:59',0,'0','5','5'),(3,8,'2024-01-26 09:05:41',0,'0','8','8'),(4,11,'2024-02-01 10:01:41',0,'0','11','11'),(5,14,'2024-02-04 09:09:01',0,'0','14','14'),(6,14,'2024-02-04 09:19:17',0,'0','14','14');
+/*!40000 ALTER TABLE `operation_time_1` ENABLE KEYS */;
+UNLOCK TABLES;
+SET @@SESSION.SQL_LOG_BIN = @MYSQLDUMP_TEMP_LOG_BIN;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-02-05 11:39:12
